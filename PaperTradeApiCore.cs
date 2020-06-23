213d800a9430856f1c7cab7d789be396c3f127f0
TimHanewich <57418795+TimHanewich@users.noreply.github.com>
2020-06-23 13:23:59 -0400

    Changed return type from string to HttpResponseMessage

--- PaperTradeApiCore.cs
+++ PaperTradeApiCore.cs
@@ -8,20 +8,26 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 using Yahoo.Finance;
+using System.Net.Http;
+using System.Net;
 
 namespace PaperTradeApi
 {
     public static class PaperTradeApiCore
     {
+        private const int Version = 2;
+
         [FunctionName("StockSummaryData")]
-        public async static Task<string> GetStockSummaryData([HttpTrigger(AuthorizationLevel.Anonymous, "get")] HttpRequest req, ILogger log)
+        public async static Task<HttpResponseMessage> GetStockSummaryData([HttpTrigger(AuthorizationLevel.Anonymous, "get")] HttpRequest req, ILogger log)
         {
             log.LogInformation("New request received");
             string symbol = req.Query["symbol"];
             if (symbol == null || symbol == "")
             {
                 log.LogInformation("Symbol not present as part of request. Ending.");
-                return "Fatal failure. Parameter 'symbol' not provided. You must provide a stock symbol to return data for.";
+                HttpResponseMessage hrm = new HttpResponseMessage(HttpStatusCode.BadRequest);
+                hrm.Content = new StringContent("Fatal failure. Parameter 'symbol' not provided. You must provide a stock symbol to return data for.");
+                return hrm;
             }
 
 
@@ -34,13 +40,18 @@ namespace PaperTradeApi
                 log.LogInformation("Serializing");
                 string json = JsonConvert.SerializeObject(e.Summary);
                 log.LogInformation("Returning");
-                return json;
+
+                HttpResponseMessage hrm = new HttpResponseMessage(HttpStatusCode.OK);
+                hrm.Content = new StringContent(json);
+                return hrm;
             }
             catch (Exception e)
             {
                 string err_msg = "Fatal failure while accessing summary data for stock '" + symbol.ToUpper() + "'. Internal error message: " + e.Message;
                 log.LogInformation(err_msg);
-                return err_msg;
+                HttpResponseMessage hrm = new HttpResponseMessage(HttpStatusCode.BadRequest);
+                hrm.Content = new StringContent(err_msg);
+                return hrm;
             }
         }
 
