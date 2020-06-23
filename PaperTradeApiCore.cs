548b4c1a441ff98c85478bdbce74ec054a9c6bce
TimHanewich <57418795+TimHanewich@users.noreply.github.com>
2020-06-23 14:34:43 -0400

    Added multiple-stock method

--- PaperTradeApiCore.cs
+++ PaperTradeApiCore.cs
@@ -11,6 +11,9 @@ using Yahoo.Finance;
 using System.Net.Http;
 using System.Net;
 using System.Text;
+using System.Collections.Generic;
+
+
 namespace PaperTradeApi
 {
     public static class PaperTradeApiCore
@@ -55,6 +58,74 @@ namespace PaperTradeApi
             }
         }
 
+        [FunctionName("MultipleStockSummaryData")]
+        public async static Task<HttpResponseMessage> GetMultipleStockSummaryData([HttpTrigger(AuthorizationLevel.Anonymous, "post")] HttpRequest req, ILogger log)
+        {
+            StreamReader sr = new StreamReader(req.Body);
+            string content = await sr.ReadToEndAsync();
+
+            //Check for blank
+            if (content == null || content == "")
+            {
+                HttpResponseMessage hrm = new HttpResponseMessage(HttpStatusCode.BadRequest);
+                hrm.Content = new StringContent("Your request body was blank. Please include a body of an array of strings (stock symbols), encoded in JSON format.");
+                return hrm;
+            }
+
+            //Deserialize the body into an array of strings
+            string[] symbols = null;
+            try
+            {
+                symbols = JsonConvert.DeserializeObject<string[]>(content);
+            }
+            catch
+            {
+                HttpResponseMessage hrm = new HttpResponseMessage(HttpStatusCode.BadRequest);
+                hrm.Content = new StringContent("Fatal error while parsing request body to JSON string array.");
+                return hrm;
+            }
+            
+
+            //Set them up
+            HttpClient hc = new HttpClient();
+            List<Task<HttpResponseMessage>> Requests = new List<Task<HttpResponseMessage>>();
+            foreach (string s in symbols)
+            {
+                string url = "http://papertradesim.azurewebsites.net/api/StockSummaryData?symbol=" + s.Trim().ToLower();
+                Requests.Add(hc.GetAsync(url));
+            }
+
+            //Wait for all responses
+            HttpResponseMessage[] responses = await Task.WhenAll(Requests);
+
+
+            //Parse into all
+            List<EquitySummaryData> datas = new List<EquitySummaryData>();
+            foreach (HttpResponseMessage hrm in responses)
+            {
+                if (hrm.StatusCode == HttpStatusCode.OK)
+                {
+                    try
+                    {
+                        string cont = await hrm.Content.ReadAsStringAsync();
+                        EquitySummaryData esd = JsonConvert.DeserializeObject<EquitySummaryData>(cont);
+                        datas.Add(esd);
+                    }
+                    catch
+                    {
+                        
+                    }
+                }
+            }
+
+
+
+            //return as Json
+            string as_json = JsonConvert.SerializeObject(datas.ToArray());
+            HttpResponseMessage rhrm = new HttpResponseMessage(HttpStatusCode.OK);
+            rhrm.Content = new StringContent(as_json, Encoding.UTF8, "application/json");
+            return rhrm;
+        }
 
     }
 }
